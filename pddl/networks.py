298e0c881d734465224a039724ddbbac3b75e5f0
Meraj Hassanzadeh <129551905+imRaajee@users.noreply.github.com>
2026-04-16 12:36:32 +0330

    Update networks.py

--- pddl/networks.py
+++ pddl/networks.py
@@ -6,12 +6,6 @@ import torch
 import torch.nn as nn
 from config import *
 
-def weights_init(m):
-    """Xavier uniform initialization"""
-    if isinstance(m, nn.Linear):
-        torch.nn.init.xavier_uniform_(m.weight.data)
-        torch.nn.init.zeros_(m.bias.data)
-
 class UnifiedDNN(nn.Module):
     def __init__(self, dim_in, dim_out, n_layer, n_node, ub, lb, activation=nn.Tanh(), 
                  output_activation=None):
@@ -24,7 +18,6 @@ class UnifiedDNN(nn.Module):
         self.ub = torch.tensor(ub, dtype=torch.float).to(device)
         self.lb = torch.tensor(lb, dtype=torch.float).to(device)
         self.output_activation = output_activation
-        self.net.apply(weights_init)
 
     def forward(self, x):
         out = self.net(x)
